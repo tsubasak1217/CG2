bb52aa3bef3fc949449f8b9ad6ce81a0f47eb2d4
tsubasak1217 <k023g0112@g.neec.ac.jp>
2024-04-18 00:06:19 +0900

    save

--- CG2_0/main.cpp
+++ CG2_0/main.cpp
@@ -4,8 +4,10 @@
 #include <cassert>
 #include <d3d12.h>
 #include <dxgi1_6.h>
+#include <dxgidebug.h>
 #pragma comment(lib,"d3d12.lib")
 #pragma comment(lib,"dxgi.lib")
+#pragma comment(lib,"dxguid.lib")
 
 LRESULT CALLBACK WndProc(
     HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam
@@ -370,6 +372,34 @@ int WINAPI WinMain(
 
 
         }
+    }
+
+    // オブジェクト類の解放
+    CloseHandle(fenceEvent);
+    fence->Release();
+    rtvDescriptorHeap->Release();
+    swapChainResources[0]->Release();
+    swapChainResources[1]->Release();
+    swapChain->Release();
+    commandList->Release();
+    commandAllocator->Release();
+    commandQueue->Release();
+    device->Release();
+    useAdapter->Release();
+    dxgiFactory->Release();
+#ifdef _DEBUG
+    //debugController->Release();
+#endif
+    CloseWindow(hwnd);
+
+
+    // 解放漏れがないかチェック
+    IDXGIDebug1* debug;
+    if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&debug)))) {
+        debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
+        debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
+        debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
+        debug->Release();
     }
 
     return 0;
