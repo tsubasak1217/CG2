4b3583da400d61e2dd01b015e8885ab5893b25f2
tsubasak1217 <k023g0112@g.neec.ac.jp>
2024-06-05 11:03:52 +0900

    save

---
+++ CG2_0/SEED/lib/math/myFunc/ShapeMath.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "include_shapes.h"
+#include "Vector3.h"
+#include "Vector2.h"
+#include <stdint.h>
+#include <cmath>
+#include <numbers>
+
+//---------------------- 正三角形 -------------------------
+
+Triangle MakeEqualTriangle(float radius, uint32_t color);
+Triangle2D MakeEqualTriangle2D(float radius, uint32_t color);
